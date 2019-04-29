0154b110dfef5f32e706c84e58212ab74914eb61
elenore29 <elenore.nava@gmail.com>
2019-04-29 11:40:33 -0500

    Updates

---
+++ precache-manifest.cc3c5064ef564f2b7c357bbf8d908f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-second-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ddcf177824aa58e6529f",
+    "url": "/my-second-react-app/static/js/runtime~main.7eb22c46.js"
+  },
+  {
+    "revision": "6ca479fd023a736ddde1",
+    "url": "/my-second-react-app/static/js/main.ad96c28c.chunk.js"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/my-second-react-app/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "6ca479fd023a736ddde1",
+    "url": "/my-second-react-app/static/css/main.008407a9.chunk.css"
+  },
+  {
+    "revision": "2ee83eb0edb9bbbcd6e22d4d06ebef51",
+    "url": "/my-second-react-app/index.html"
+  }
+];
